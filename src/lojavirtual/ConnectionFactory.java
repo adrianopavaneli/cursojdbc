5ff9b4dde98c36f75974c7daeecd66f76010f969
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-09-25 15:04:02 -0300

    Pool de Conexões

--- src/lojavirtual/ConnectionFactory.java
+++ src/lojavirtual/ConnectionFactory.java
@@ -1,17 +1,28 @@
 package lojavirtual;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class ConnectionFactory {
-	public Connection recuperarConexao() throws SQLException {
-		return DriverManager
-				.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "swxaqz123");
-		
-		
-		
+
+	public DataSource dataSource;
+
+	public ConnectionFactory() {
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("root");
+		comboPooledDataSource.setPassword("swxaqz123");
+
+		this.dataSource = comboPooledDataSource;
 	}
-	
+
+	public Connection recuperarConexao() throws SQLException {
+		return this.dataSource.getConnection();
+
+	}
 
 }
