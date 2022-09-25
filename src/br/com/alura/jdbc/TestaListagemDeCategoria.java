4945c82538ddd1637afc1ca2d1d25df762183556
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-09-25 18:14:34 -0300

    Utilizando inner join

---
+++ src/br/com/alura/jdbc/TestaListagemDeCategoria.java
@@ -0,0 +1,32 @@
+package br.com.alura.jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import br.com.alura.jdbc.dao.CategoriaDAO;
+import br.com.alura.jdbc.dao.ConnectionFactory;
+import br.com.alura.jdbc.modelo.Categoria;
+import br.com.alura.jdbc.modelo.Produto;
+
+public class TestaListagemDeCategoria {
+	public static void main(String[] args) throws SQLException {
+		try(Connection connection = new ConnectionFactory().recuperarConexao()){
+		
+		CategoriaDAO categoriaDAO = new CategoriaDAO(connection);
+		List<Categoria> listaDeCategorias = categoriaDAO.listarComProdutos();
+		listaDeCategorias.stream().forEach(lc -> {
+			System.out.println(lc.getNome());
+			for (Produto produto : lc.getProdutos()) {
+				System.out.println(lc.getNome() + " - " + produto.getNome());
+			}
+		});
+		
+		
+		
+		
+		
+		}
+	}
+
+}
